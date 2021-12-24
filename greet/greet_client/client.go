f6461b105d2eaeba718db0b97e3ff7435b9585b9
Lucas Thiago Batista <lucas.batista@santander.com.br>
2021-12-24 19:44:36 -0300

    add server streaming version to client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/ltbatista/grpc-go-course/greet/greetpb"
@@ -20,6 +21,7 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(cc)
 	doUnary(c)
+	doServerStreaming(c)
 
 }
 
@@ -36,4 +38,29 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error while calling Greet RPC:  %v", err)
 	}
 	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Sarting to do an Unary RPC...")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Jorge",
+			LastName:  "Benjor",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes RPC: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// chegamos ao fim do streaming
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
+	}
 }
